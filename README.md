28b483b7c6bde02a158c4ff8683852e617fcb0e4
justforuse <allenforuse@gmail.com>
2021-11-23 22:11:14 +0800

    :tada: init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quick Search
+
+A useful(I guess) extension for web developers, it contains several well-known websites, for now, it includes mdn, npm, juejin and github.
+
+If you want add a website, you can make a PR.
+
+## How to use
+
+- Install this extension
+- Input 'qs' in url box and press tab
+- Select the suggested options
---
+++ background.js
@@ -0,0 +1,40 @@
+const webList = [
+    {
+        name: 'MDN',
+        homepage: 'https://developer.mozilla.org/zh-CN/',
+        searchUrl: 'https://developer.mozilla.org/zh-CN/search?q=',
+    },
+    {
+        name: 'NPM',
+        homepage: 'https://www.npmjs.com/',
+        searchUrl: 'https://www.npmjs.com/search?q=',
+    },
+    {
+        name: 'Jue Jin',
+        homepage: 'https://juejin.im/',
+        searchUrl: 'https://juejin.im/search?query=',
+    },
+    {
+        name: 'GitHub',
+        homepage: 'https://github.com/',
+        searchUrl: 'https://github.com/search?q='
+    }
+]
+
+// listen url input, and set the suggest results
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+    // generate suggest results
+    const res = webList.map(item => {
+        return {
+            content: item.searchUrl + text,
+            description: `<match>${item.name}</match>: ${text}`,
+        }
+    })
+    suggest(res)
+})
+
+chrome.omnibox.onInputEntered.addListener((text, OnInputEnteredDisposition) => {
+    chrome.tabs.update({
+        url: text
+    })
+})
